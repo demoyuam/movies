59687322c77468a47c5d227045691e2281cf44c3
Y.A. <130000297+demoyuam@users.noreply.github.com>
2024-10-22 14:30:38 +0300

    Update LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,49 +1,45 @@
 "use client";
 
-import Image from "next/image";
 import { useInView } from "react-intersection-observer";
 import { useEffect, useState } from "react";
-
 import { fetchAnime } from "../app/action";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+import Image from "next/image";
 
 let page = 2;
 
-export type AnimeCard = JSX.Element;
-
 function LoadMore() {
   const { ref, inView } = useInView();
-
-  const [data, setData] = useState<AnimeCard[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (inView) {
       setIsLoading(true);
-      // Add a delay of 500 milliseconds
       const delay = 500;
 
       const timeoutId = setTimeout(() => {
         fetchAnime(page).then((res) => {
-          setData([...data, ...res]);
+          setData((prevData) => [...prevData, ...res]);
           page++;
+          setIsLoading(false);
         });
-
-        setIsLoading(false);
       }, delay);
 
-      // Clear the timeout if the component is unmounted or inView becomes false
       return () => clearTimeout(timeoutId);
     }
-  }, [inView, data, isLoading]);
+  }, [inView]);
 
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data}
+        {data.map((anime, index) => (
+          <AnimeCard key={`${anime.id}-${index}`} anime={anime} index={index} />
+        ))}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
-          {inView && isLoading && (
+          {isLoading && (
             <Image
               src="./spinner.svg"
               alt="spinner"
