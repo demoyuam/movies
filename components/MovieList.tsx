8266e78b06e9f6c94ee77dcaedd306bb744a495c
Y.A. <130000297+demoyuam@users.noreply.github.com>
2024-10-22 15:14:58 +0300

    Update MovieList.tsx

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -28,27 +28,29 @@ function MovieList({ initialData }: MovieListProps) {
 
   return (
     <div className="flex flex-col gap-5">
-      <input
-        type="text"
-        placeholder="Поиск по названию..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        className="p-2 rounded border border-gray-300 text-black"
-      />
+      <div className="flex flex-col sm:flex-row gap-4 items-center">
+        <input
+          type="text"
+          placeholder="Search by title..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 rounded border border-gray-300 bg-gray-800 text-gray-200 w-full max-w-md"
+        />
 
-      <select
-        value={sortOption}
-        onChange={(e) => setSortOption(e.target.value)}
-        className="p-2 rounded border border-gray-300 text-black"
-      >
-        <option value="">Без сортировки</option>
-        <option value="title">Сортировать по названию</option>
-        <option value="rating">Сортировать по рейтингу</option>
-        <option value="year">Сортировать по году</option>
-      </select>
+        <select
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+          className="p-2 rounded border border-gray-300 bg-gray-800 text-gray-200 w-full max-w-md"
+        >
+          <option value="">No sorting</option>
+          <option value="title">Sort by title</option>
+          <option value="rating">Sort by rating</option>
+          <option value="year">Sort by year</option>
+        </select>
+      </div>
 
       {sortedData.length === 0 ? (
-        <p className="text-white">Фильмы не найдены.</p>
+        <p className="text-white">Movies not found.</p>
       ) : (
         <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
           {sortedData.map((movie, index) => (
