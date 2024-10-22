a16016fd3d58a024ee40b59ea448e236d5d09cbe
Y.A. <130000297+demoyuam@users.noreply.github.com>
2024-10-22 14:19:37 +0300

    Update action.tsx

--- app/action.tsx
+++ app/action.tsx
@@ -1,17 +1,15 @@
 "use server";
 
-import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+import { AnimeProp } from "@/components/AnimeCard";
 
 const MAX_LIMIT = 8;
 
-export async function fetchAnime(page: number) {
+export async function fetchAnime(page: number): Promise<AnimeProp[]> {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
   );
 
   const data = await response.json();
 
-  return data.map((anime: AnimeProp, index: number) => (
-    <AnimeCard key={anime.id} anime={anime} index={index} />
-  ));
+  return data;
 }
