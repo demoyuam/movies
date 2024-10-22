d16045849bac0bfac5d0a024d28d6f5a9994f6c0
Y.A. <130000297+demoyuam@users.noreply.github.com>
2024-10-22 15:46:07 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import MovieList from "../components/MovieList";
 async function Home() {
   const data = await fetchMovies();
 
+  
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Movies</h2>
